ac92f9d2c56f9d0ddb27f631d079344e469e20d6
hexbreak <theownerproject@gmail.com>
2020-12-22 01:34:50 +0000

    Updates

--- src/main.js
+++ src/main.js
@@ -5,6 +5,43 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+// const send = document.querySelector(" ");
+const cancel = document.querySelector(".btn-secondary");
+const allInputs = document.querySelectorAll(".form-control");
+// const errorTab = document.querySelector(" ");
+
+function validation() {
+  var card = document.querySelector("#cardnumber");
+  if (card == "") {
+  }
+  var ccv = document.querySelector("#ccv");
+
+  var amount = document.querySelector("#amount");
+
+  var firstname = document.querySelector("#firstname");
+
+  var lastname = document.querySelector("#lastname");
+
+  var city = document.querySelector("#city");
+
+  var state = document.querySelector("#select-state");
+
+  var postalcode = document.querySelector("#postalcode");
+}
+
+// examples to follow
+/*
+function validateForm() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+*/
+
+/*
+  card.addEventListener("submit", e => {
+    e.preventDefault();
+  });
+*/
