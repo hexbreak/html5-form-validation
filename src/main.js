9c62ef49a6812441aefe05f9846918ee24d8eba6
hexbreak <theownerproject@gmail.com>
2020-12-22 01:39:26 +0000

    Updates

--- src/main.js
+++ src/main.js
@@ -11,23 +11,21 @@ const allInputs = document.querySelectorAll(".form-control");
 // const errorTab = document.querySelector(" ");
 
 function validation() {
-  var card = document.querySelector("#cardnumber");
-  if (card == "") {
-  }
+  var cardnumber = document.querySelector("#cardnumber");
   var ccv = document.querySelector("#ccv");
-
   var amount = document.querySelector("#amount");
-
   var firstname = document.querySelector("#firstname");
-
   var lastname = document.querySelector("#lastname");
-
   var city = document.querySelector("#city");
-
   var state = document.querySelector("#select-state");
-
   var postalcode = document.querySelector("#postalcode");
+
+  if (cardnumber.value<1000000000000000) {
+      displayError('Card Number must be 16 digits.')
+  }
 }
+
+
 
 // examples to follow
 /*
