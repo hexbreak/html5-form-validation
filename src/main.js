d0563c04cced4a22f63076d436b5d0e1c2ddbc32
hexbreak <theownerproject@gmail.com>
2020-12-22 01:52:20 +0000

    Updates

--- src/main.js
+++ src/main.js
@@ -20,12 +20,39 @@ function validation() {
   var state = document.querySelector("#select-state");
   var postalcode = document.querySelector("#postalcode");
 
-  if (cardnumber.value<1000000000000000) {
-      displayError('Card Number must be 16 digits.')
+  if (cardnumber.value < 1000000000000000) {
+    displayError("Card Number must be 16 digits.");
+    cardnumber.classList.add("invalid");
+  }
+  if (ccv.value.length != 3) {
+    displayError("CCV must be 3 digits.");
+    ccv.classList.add("invalid");
+  }
+  if (amount.value.length != 0) {
+    displayError("Amount required.");
+    amount.classList.add("invalid");
+  }
+  if (firstname.value.length != 1) {
+    displayError("First Name required.");
+    amount.classList.add("invalid");
+  }
+  if (lastname.value.length != 0) {
+    displayError("Last Name required.");
+    amount.classList.add("invalid");
+  }
+  if (city.value.length != 0) {
+    displayError("City required.");
+    amount.classList.add("invalid");
+  }
+  if (state.value === "Choose...") {
+    displayError("State required.");
+    amount.classList.add("invalid");
+  }
+  if (postalcode.value.length != 5) {
+    displayError("Postal Code required.");
+    amount.classList.add("invalid");
   }
 }
-
-
 
 // examples to follow
 /*
